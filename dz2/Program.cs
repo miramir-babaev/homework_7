2dbefa20658601e29e43e28ec85ea9eebadf77b5
Miramir Babaev <miramir.babaev@yandex.ru>
2023-07-15 13:36:42 +0300

    github

---
+++ dz2/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Write("Введите номер строки -> ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер столбца -> ");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[10, 10];
+FillArrayRandomNumbers(numbers);
+
+if (rows > numbers.GetLength(0) || cols > numbers.GetLength(1))
+{
+    Console.WriteLine("Такого элемента нет");
+}
+else
+{
+    Console.WriteLine($"Значение элемента {rows} строки и {cols} столбца равно - {numbers[rows - 1, cols - 1]}");
+}
+
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-100, 100) / 10;
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write(" ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write(" ");
+        Console.WriteLine("");
+    }
+}
---
+++ dz3/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.Write("Введите количество строк -> ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов -> ");
+int cols = Convert.ToInt32(Console.ReadLine());
+
+int[,] numbers = new int[rows, cols];
+FillArrayRandomNumbers(numbers);
+
+
+for (int j = 0; j < numbers.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        avarage = (avarage + numbers[i, j]);
+    }
+    avarage = avarage / rows;
+    Console.Write(avarage + " ");
+}
+Console.WriteLine();
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write(" ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write(" ");
+        Console.WriteLine("");
+    }
+}
